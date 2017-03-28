1e02b69514325579e21dcff36dbf7f1c6c78566d
Adibi <maadibi@ead.ubc.ca>
2017-03-28 14:58:57 -0700

    Data files moved to data-raw, for internal implementation. An R file wthin data-raw was created to document embedding of internal data.

---
+++ data-raw/data-raw.R
@@ -0,0 +1,6 @@
+# Internal Data Documentation
+# This document outlines how internal data were embedded within the package
+
+CanSim.105.0501<-read.csv("./CanSim.105.0501.csv", header = T); #used for validation
+CanSim.052.0005<-read.csv("./CanSim.052.0005", header = T); #used for validation
+devtools::use_data(CanSim.105.0501, CanSim.052.0005, internal = TRUE)
