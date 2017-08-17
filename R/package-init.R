6cc6f44fa622b46e6d438a730373584d1d376d69
Amin Adibi <adibi@alumni.ubc.ca>
2017-08-17 15:10:32 -0700

    added imports for stats and graphics

--- R/package-init.R
+++ R/package-init.R
@@ -1,4 +1,6 @@
 #' @exportPattern("^[^\\.]")
 #' @useDynLib epicR
 #' @importFrom Rcpp sourceCpp
+#' @importFrom graphics barplot legend lines pie plot title
+#' @importFrom stats aggregate binomial coefficients glm optim runif
 NULL
