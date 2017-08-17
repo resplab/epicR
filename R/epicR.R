b1e1eb68d27ac40dd77d0cdd7a2faee1300826cc
Amin Adibi <adibi@alumni.ubc.ca>
2017-08-17 11:48:14 -0700

    Handling devtool::check() errors

---
+++ R/epicR.R
@@ -0,0 +1,13 @@
+#' \code{epicR} package
+#'
+#' Evaluation Platform in COPD (EPIC) R Package
+#'
+#' See the README on
+#' \href{https://github.com/aminadibi/epicR#readme}{GitHub}
+#'
+#' @docType package
+#' @name epicR
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
