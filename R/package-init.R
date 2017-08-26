3bf03d9e00161e514a687ee4d72d4f9a7279692c
Amin Adibi <adibi@alumni.ubc.ca>
2017-08-25 17:22:02 -0700

    packageVersion now imported

--- R/package-init.R
+++ R/package-init.R
@@ -3,4 +3,5 @@
 #' @importFrom Rcpp sourceCpp
 #' @importFrom graphics barplot legend lines pie plot title
 #' @importFrom stats aggregate binomial coefficients glm optim runif confint
+#' @importFrom utils packageVersion
 NULL
