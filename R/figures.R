be41ffc277fb1a9cae9777daef123936fdcb221b
Amin Adibi <adibi@alumni.ubc.ca>
2017-08-28 17:17:11 -0700

    added "FEV1_by_sex_year" to excel

--- R/figures.R
+++ R/figures.R
@@ -64,11 +64,11 @@ export_figures <- function(nPatients = 10^4) {
   df <- as.data.frame(COPD_inc_by_sex)
   dfm <- reshape2::melt(df[,c('Year','Male','Female')], id.vars = 1)
   plot_COPD_inc_by_sex <- ggplot2::ggplot(dfm, aes(x = Year, y = value)) +
-    geom_bar(aes(fill = variable), stat = "identity", position = "dodge") + ylim(low=0, high=0.5)
+    geom_bar(aes(fill = variable), stat = "identity", position = "dodge") + ylim(low=0, high=0.5) + labs(title = "Incidence of COPD") + ylab ("COPD Incidence") + labs(caption = "(based on population at age 40 and above)")
 
   print(plot_COPD_inc_by_sex) #plot needs to be showing
   openxlsx::insertPlot(wb, "COPD_incidence_by_year_sex",  xy = c("J", 10), width = 20, height = 13.2 , fileType = "png", units = "cm")
-
+#  rm(COPD_inc_by_sex, data_COPD_inc, plot_COPD_inc_by_sex)
 
   ## COPD Prevalence per year and sex
   COPD_prev_by_sex <- matrix (NA, nrow = input$global_parameters$time_horizon, ncol = 3)
@@ -80,10 +80,40 @@ export_figures <- function(nPatients = 10^4) {
   df <- as.data.frame(COPD_prev_by_sex)
   dfm <- reshape2::melt(df[,c('Year','Male','Female')],id.vars = 1)
   plot_COPD_prev_by_sex <- ggplot2::ggplot(dfm, aes(x = Year, y = value)) +
-    geom_bar(aes(fill = variable), stat = "identity", position = "dodge") + ylim(low=0, high=0.5)
+    geom_bar(aes(fill = variable), stat = "identity", position = "dodge") + ylim(low=0, high=0.5) + labs(title = "Prevalence of COPD") + ylab ("Prevalence") + labs(caption = "(based on population at age 40 and above)")
 
   print(plot_COPD_prev_by_sex) #plot needs to be showing
   openxlsx::insertPlot(wb, "COPD_prevalence_by_year_sex",  xy = c("J", 10), width = 20, height = 13.2 , fileType = "png", units = "cm")
+
+
+  ## FEV1 by sex and COPD year
+  fev1_by_sex <- matrix (NA, nrow = input$global_parameters$time_horizon+1, ncol = 4)
+  colnames(fev1_by_sex) <- c("Year_with_COPD", "Male", "Female", "All")
+  fev1_by_sex[1:(input$global_parameters$time_horizon+1), 1] <- 1:(input$global_parameters$time_horizon+1)
+  data_COPD <- subset (data, gold > 0)
+
+  for (i in 0:input$global_parameters$time_horizon) {
+    temp_data_COPD_years <- subset(data, ((followup_after_COPD == i) & (sex == 0)))
+    fev1_by_sex[i+1, 2] <- mean (temp_data_COPD_years[,"FEV1"])
+
+    temp_data_COPD_years <- subset(data, ((followup_after_COPD == i) & (sex == 1)))
+    fev1_by_sex[i+1, 3] <- mean (temp_data_COPD_years[,"FEV1"])
+
+    temp_data_COPD_years <- subset(data, followup_after_COPD == i)
+    fev1_by_sex[i+1, 4] <- mean (temp_data_COPD_years[,"FEV1"])
+  }
+
+  openxlsx::writeData(wb, "FEV1_by_sex_year", fev1_by_sex, startCol = 2, startRow = 3, colNames = TRUE)
+
+  df <- as.data.frame(fev1_by_sex)
+  dfm <- reshape2::melt(df[,c('Year_with_COPD','Male','Female', "All")],id.vars = 1)
+#  plot_fev1_by_sex <- ggplot2::ggplot(dfm, aes(x = Year_with_COPD, y = value)) +
+  #  geom_point(aes(), stat = "identity", position = "dodge") + ylim(low=0, high=0.5)
+  plot_fev1_by_sex <- ggplot2::ggplot(dfm, aes(Year_with_COPD, value, colour = variable)) +
+    geom_point() + labs(title = "Mean FEV1 by Number of Years with COPD") + ylab ("FEV1 (L)")
+  print(plot_fev1_by_sex) #plot needs to be showing
+  openxlsx::insertPlot(wb, "FEV1_by_sex_year",  xy = c("J", 10), width = 20, height = 13.2 , fileType = "png", units = "cm")
+
 
   ## Save workbook
   ## Open in excel without saving file: openXL(wb)
