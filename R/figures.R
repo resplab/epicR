f0a279a54b671c364def98ed2a6f3ae7b0daaf08
Amin Adibi <adibi@alumni.ubc.ca>
2017-08-29 15:59:35 -0700

    added prevalence of COPD by age group

--- R/figures.R
+++ R/figures.R
@@ -46,7 +46,7 @@ export_figures <- function(nPatients = 10^4) {
 
   ## Populate workbooks
 
-  ## COPD Incidence per year and sex
+  ####################################################### COPD Incidence per year and sex  #####################################################
   COPD_inc_by_sex <- matrix (NA, nrow = input$global_parameters$time_horizon, ncol = 3)
   colnames(COPD_inc_by_sex) <- c("Year", "Male", "Female")
   COPD_inc_by_sex[1:input$global_parameters$time_horizon, 1] <- c(2015:(2015+input$global_parameters$time_horizon-1))
@@ -70,7 +70,11 @@ export_figures <- function(nPatients = 10^4) {
   openxlsx::insertPlot(wb, "COPD_incidence_by_year_sex",  xy = c("J", 10), width = 20, height = 13.2 , fileType = "png", units = "cm")
 #  rm(COPD_inc_by_sex, data_COPD_inc, plot_COPD_inc_by_sex)
 
-  ## COPD Prevalence per year and sex
+
+
+
+
+  ####################################################### COPD Prevalence per year and sex  #####################################################
   COPD_prev_by_sex <- matrix (NA, nrow = input$global_parameters$time_horizon, ncol = 3)
   colnames(COPD_prev_by_sex) <- c("Year", "Male", "Female")
   COPD_prev_by_sex[1:input$global_parameters$time_horizon, 1] <- c(2015:(2015+input$global_parameters$time_horizon-1))
@@ -86,7 +90,50 @@ export_figures <- function(nPatients = 10^4) {
   openxlsx::insertPlot(wb, "COPD_prevalence_by_year_sex",  xy = c("J", 10), width = 20, height = 13.2 , fileType = "png", units = "cm")
 
 
-  ## FEV1 by sex and COPD year
+
+  ##################################################### COPD Prevalence by Age Group and Sex #####################################################
+
+  COPD_by_ctime_age <- as.data.frame(op_ex$n_COPD_by_ctime_age)
+  alive_by_ctime_age <- as.data.frame(op_ex$n_alive_by_ctime_age)
+  COPD_prev_by_agegroup <- matrix (0, nrow = 6, ncol = 1) #40-50, 50-60, 60-70, 70-80,  80-90, 90+
+  num_COPD_prev_by_agegroup <- matrix (0, nrow = 6, ncol = 1) #40-50, 50-60, 60-70, 70-80,  80-90, 90+
+  denom_COPD_prev_by_agegroup <- matrix (0, nrow = 6, ncol = 1) #40-50, 50-60, 60-70, 70-80,  80-90, 90+
+
+
+
+
+  for (i in 1:5) {
+    for (j in 0:9){
+      num_COPD_prev_by_agegroup [i] <- num_COPD_prev_by_agegroup [i] + colSums(COPD_by_ctime_age[10*(3+i)+j])
+      denom_COPD_prev_by_agegroup [i] <- denom_COPD_prev_by_agegroup [i] + colSums(alive_by_ctime_age[10*(3+i)+j])
+    }
+  }
+
+  #handling the special case of 90-110 years, ie: i=4
+  for (j in 0:20){
+    num_COPD_prev_by_agegroup [6] <- num_COPD_prev_by_agegroup [6] + colSums(COPD_by_ctime_age[10*(3+6)+j])
+    denom_COPD_prev_by_agegroup [6] <- denom_COPD_prev_by_agegroup [6] + colSums(alive_by_ctime_age[10*(3+6)+j])
+  }
+
+  COPD_prev_by_agegroup <- num_COPD_prev_by_agegroup / denom_COPD_prev_by_agegroup
+  SE_COPD_prev_by_agegroup <- sqrt (COPD_prev_by_agegroup*(1-COPD_prev_by_agegroup)/denom_COPD_prev_by_agegroup)
+
+  df <- data.frame(Age_Group = c("40-50", "50-60", "60-70", "70-80", "80-90", "90+"), Prevalence = COPD_prev_by_agegroup)
+  openxlsx::writeData(wb, "Prev_Age_Group_CanCOLD-BOLD", df, startCol = 2, startRow = 3, colNames = TRUE)
+
+  plot_COPD_prev_by_agegroup  <- ggplot2::ggplot(df, aes(x = Age_Group, y = Prevalence)) +
+    geom_bar(stat = "identity", position = "dodge", width = 0.2, fill = "#FF6666") + geom_errorbar(aes(ymin = Prevalence - 1.96*SE_COPD_prev_by_agegroup, ymax = Prevalence + 1.96*SE_COPD_prev_by_agegroup ),
+                                                                                                       width=.2, position=position_dodge(.9)) + ylim(low = 0, high = 0.5) + labs (title = "Prevalence of COPD by Age Group") + ylab ("Prevalence") + labs(caption = "(error bars represent 95% CI)")
+
+  print(plot_COPD_prev_by_agegroup) #plot needs to be showing
+  openxlsx::insertPlot(wb, "Prev_Age_Group_CanCOLD-BOLD",  xy = c("J", 10), width = 20, height = 13.2 , fileType = "png", units = "cm")
+
+
+
+
+
+  ##################################################### FEV1 by sex and COPD year #####################################################
+
   fev1_by_sex <- matrix (NA, nrow = input$global_parameters$time_horizon+1, ncol = 4)
   colnames(fev1_by_sex) <- c("Year_with_COPD", "Male", "Female", "All")
   fev1_by_sex[1:(input$global_parameters$time_horizon+1), 1] <- 1:(input$global_parameters$time_horizon+1)
@@ -115,7 +162,7 @@ export_figures <- function(nPatients = 10^4) {
   openxlsx::insertPlot(wb, "FEV1_by_sex_year",  xy = c("J", 10), width = 20, height = 13.2 , fileType = "png", units = "cm")
 
 
-  ## Save workbook
+  ####################################################### Save workbook #####################################################
   ## Open in excel without saving file: openXL(wb)
   wbfilename <- paste(Sys.Date(), " Figures EpicR ver", packageVersion("epicR"), ".xlsx")
   openxlsx::saveWorkbook(wb, wbfilename, overwrite = TRUE)
