c92bfcce989f3bcfd531a40d6d41355e891a9899
Amin Adibi <adibi@alumni.ubc.ca>
2017-09-07 11:49:11 -0700

    write.csv now imported

--- R/package-init.R
+++ R/package-init.R
@@ -4,5 +4,6 @@
 #' @importFrom graphics barplot legend lines pie plot title
 #' @importFrom stats aggregate binomial coefficients glm optim runif confint
 #' @importFrom utils packageVersion
+#' @importFrom utils write.csv
 #' @import ggplot2
 NULL
