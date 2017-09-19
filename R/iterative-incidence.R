330bc3071478844865051a3a6e37450f5371c379
Amin Adibi <adibi@alumni.ubc.ca>
2017-09-19 11:42:34 -0700

    Microsoft R compatible

--- R/iterative-incidence.R
+++ R/iterative-incidence.R
@@ -14,11 +14,11 @@ iterate_COPD_inc<-function(nIterations=500,
     ,female =c(Intercept =0, age = 0, age2 =0, pack_years = 0, smoking_status = 0, year = 0, asthma = 0))
 
 
-#  cat("iteration", "intercept_men", "age_coeff_men", "packyears_coeff_men", "intercept_women", "age_coeff_women", "packyears_coeff_women", file="iteration_coeff.csv",sep=",",append=FALSE, fill=FALSE)
-#  cat("\n",file="iteration_coeff.csv",sep=",",append=TRUE)
+  cat("iteration", "intercept_men", "age_coeff_men", "packyears_coeff_men", "intercept_women", "age_coeff_women", "packyears_coeff_women", file="iteration_coeff.csv",sep=",",append=FALSE, fill=FALSE)
+  cat("\n",file="iteration_coeff.csv",sep=",",append=TRUE)
 
-#  cat("iteration","resid_intercept_men", "resid_age_coeff_men", "resid_packyears_coeff_men", "resid_intercept_women", "resid_packyears_coeff_women" ,"resid_age_coeff_women" , file="iteration_resid.csv",sep=",",append=FALSE, fill=FALSE)
-#  cat("\n",file="iteration_resid.csv",sep=",",append=TRUE)
+  cat("iteration","resid_intercept_men", "resid_age_coeff_men", "resid_packyears_coeff_men", "resid_intercept_women", "resid_packyears_coeff_women" ,"resid_age_coeff_women" , file="iteration_resid.csv",sep=",",append=FALSE, fill=FALSE)
+  cat("\n",file="iteration_resid.csv",sep=",",append=TRUE)
 
   for (i in 1:nIterations){
 
@@ -51,15 +51,15 @@ iterate_COPD_inc<-function(nIterations=500,
     dataF[,'gold3p']<-(dataF[,'gold']>2)*1
     dataF[,'year']<-dataF[,'local_time']+dataF[,'time_at_creation']
 
-    res_male<-glm(data=dataF[which(dataF[,'sex']==0),],formula=copd~age+pack_years+smoking_status,family=binomial(link=logit))
+    res_male<-rxGlm(data=dataF[which(dataF[,'sex']==0),],formula=copd~age+pack_years,family=binomial(link=logit))
     coefficients(res_male)
 
-    res_female<-glm(data=dataF[which(dataF[,'sex']==1),],formula=copd~age+pack_years+smoking_status,family=binomial(link=logit))
+    res_female<-rxGlm(data=dataF[which(dataF[,'sex']==1),],formula=copd~age+pack_years,family=binomial(link=logit))
     coefficients(res_female)
 
     latest_COPD_prev_logit <- cbind(
-      male =c(Intercept =summary(res_male)$coefficients[1,1],age = summary(res_male)$coefficients[2,1] ,age2 = 0, pack_years = summary(res_male)$coefficients[3,1], smoking_status = summary(res_male)$coefficients[4,1],year = 0,asthma = 0)
-      ,female =c(Intercept =summary(res_female)$coefficients[1,1] ,age = summary(res_female)$coefficients[2,1], age2 =0, pack_years = summary(res_female)$coefficients[3,1], smoking_status = summary(res_female)$coefficients[4,1] ,year = 0,asthma = 0))
+      male =c(Intercept =summary(res_male)$coefficients[1,1],age = summary(res_male)$coefficients[2,1] ,age2 = 0, pack_years = summary(res_male)$coefficients[3,1], smoking_status = 0, year = 0,asthma = 0)
+      ,female =c(Intercept =summary(res_female)$coefficients[1,1] ,age = summary(res_female)$coefficients[2,1], age2 =0, pack_years = summary(res_female)$coefficients[3,1], smoking_status = 0 ,year = 0,asthma = 0))
 
     residual <-  input$COPD$logit_p_COPD_betas_by_sex-latest_COPD_prev_logit
     message(c(i, "th loop:"))
@@ -70,7 +70,7 @@ iterate_COPD_inc<-function(nIterations=500,
     cat("\n",file="iteration_resid.csv",sep=",",append=TRUE)
 
 
-    latest_COPD_inc_logit <- latest_COPD_inc_logit+residual;
+    latest_COPD_inc_logit <- latest_COPD_inc_logit + residual
 
     message ("latest inc logit is:")
     print (latest_COPD_inc_logit)
@@ -85,24 +85,24 @@ iterate_COPD_inc<-function(nIterations=500,
   iteration_resid <- readr::read_csv("./iteration_resid.csv")
 
 
-  print( ggplot2::qplot(nIterations, age_coeff_men, data=iteration_coeff, size=I(1), main = "Age Coefficient Convergence for Men") )
-  print( ggplot2::qplot(nIterations, age_coeff_women, data=iteration_coeff, size=I(1), main = "Age Coefficient Convergence for Women") )
+  print( ggplot2::qplot(iteration, age_coeff_men, data=iteration_coeff, size=I(1), main = "Age Coefficient Convergence for Men") )
+  print( ggplot2::qplot(iteration, age_coeff_women, data=iteration_coeff, size=I(1), main = "Age Coefficient Convergence for Women") )
 
-  print( ggplot2::qplot(nIterations, packyears_coeff_men, data=iteration_coeff, size=I(1), main = "Smoking (packyears) Coefficient Convergence for Men") )
-  print( ggplot2::qplot(nIterations, packyears_coeff_women, data=iteration_coeff, size=I(1), main = "Smoking (packyears) Coefficient Convergence for women") )
+  print( ggplot2::qplot(iteration, packyears_coeff_men, data=iteration_coeff, size=I(1), main = "Smoking (packyears) Coefficient Convergence for Men") )
+  print( ggplot2::qplot(iteration, packyears_coeff_women, data=iteration_coeff, size=I(1), main = "Smoking (packyears) Coefficient Convergence for women") )
 
-  print( ggplot2::qplot(nIterations, intercept_men, data=iteration_coeff, size=I(1), main = "Logit intercept Convergence for Men") )
-  print( ggplot2::qplot(nIterations, intercept_women, data=iteration_coeff, size=I(1), main = "Logit intercept Convergence for women") )
+  print( ggplot2::qplot(iteration, intercept_men, data=iteration_coeff, size=I(1), main = "Logit intercept Convergence for Men") )
+  print( ggplot2::qplot(iteration, intercept_women, data=iteration_coeff, size=I(1), main = "Logit intercept Convergence for women") )
 
 
-  print( ggplot2::qplot(nIterations, resid_age_coeff_men, data=iteration_resid, size=I(1), main = "Residue for Age Coefficient - Men") )
-  print( ggplot2::qplot(nIterations, resid_age_coeff_women, data=iteration_resid, size=I(1), main = "Residue for Age Coefficient - Women") )
+  print( ggplot2::qplot(iteration, resid_age_coeff_men, data=iteration_resid, size=I(1), main = "Residue for Age Coefficient - Men") )
+  print( ggplot2::qplot(iteration, resid_age_coeff_women, data=iteration_resid, size=I(1), main = "Residue for Age Coefficient - Women") )
 
-  print( ggplot2::qplot(nIterations, resid_packyears_coeff_men, data=iteration_resid, size=I(1),  main = "Residue for Cigarette Smoking (packyears) Coefficient - Men") )
-  print( ggplot2::qplot(nIterations, resid_packyears_coeff_women, data=iteration_resid, size=I(1),  main = "Residue for Cigarette Smoking (packyears) Coefficient - Women") )
+  print( ggplot2::qplot(iteration, resid_packyears_coeff_men, data=iteration_resid, size=I(1),  main = "Residue for Cigarette Smoking (packyears) Coefficient - Men") )
+  print( ggplot2::qplot(iteration, resid_packyears_coeff_women, data=iteration_resid, size=I(1),  main = "Residue for Cigarette Smoking (packyears) Coefficient - Women") )
 
-  print( ggplot2::qplot(nIterations, resid_intercept_men, data=iteration_resid, size=I(1),  main = "Residue for logit intercept - Men") )
-  print( ggplot2::qplot(nIterations, resid_intercept_women, data=iteration_resid, size=I(1),  main = "Residue for logit intercept - Women") )
+  print( ggplot2::qplot(iteration, resid_intercept_men, data=iteration_resid, size=I(1),  main = "Residue for logit intercept - Men") )
+  print( ggplot2::qplot(iteration, resid_intercept_women, data=iteration_resid, size=I(1),  main = "Residue for logit intercept - Women") )
 
 
   res_male<-glm(data=dataF[which(dataF[,'sex']==0),],formula=copd~age+pack_years+year,family=binomial(link=logit))
