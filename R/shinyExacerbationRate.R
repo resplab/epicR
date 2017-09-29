a0cbcc649f360bdef1742dae9a159e24437c1a19
Hill <ahill18@ead.ubc.ca>
2017-09-29 16:53:20 -0700

    Editing shiny app

---
+++ R/shinyExacerbationRate.R
@@ -0,0 +1,191 @@
+#' @export
+report_exacerbation_by_time_base <- function(n_sim=10^5){
+  settings <- default_settings
+  settings$record_mode <- record_mode["record_mode_none"]
+  settings$agent_stack_size <- 0
+  settings$n_base_agents <- n_sim
+  settings$event_stack_size <- settings$n_base_agents * 0
+
+  init_session(settings = settings)
+
+  run()
+
+  op <- Cget_output()
+  opx <- Cget_output_ex()
+
+  cat("Annual rate of exacerbation:", sum(opx$n_exac_by_ctime_severity)/(op$cumul_time - opx$cumul_non_COPD_time), "\n")
+  cat("Annual rate of exacerbation by severity:", colSums(opx$n_exac_by_ctime_severity)/(op$cumul_time - opx$cumul_non_COPD_time),
+      "\n")
+
+  exacerbation <- list("op"=op,"opx"=opx, "settings"=settings)
+  terminate_session()
+
+  return(exacerbation)
+
+}
+
+# Annual exacerbation rate over 20 years all population
+report_exacerbation_by_time_entire_all <- function(exacerbation){
+
+  opx <- exacerbation$opx
+  y <- rowSums(opx$n_exac_by_ctime_severity)/rowSums(opx$n_alive_by_ctime_sex)
+  x <- 2015:2034
+
+  data <- data.frame(x,y)
+  names(data) <- c("year", "incidence")
+  #p <- plot(2015:2034, y, type = "l", ylim = c(0, max(y)), xlab = "Year", ylab = "Annual incidence")
+  #title(cex.main = 0.5, "Exacerbation rate over time (across population)")
+  p <- plot_ly(data, x= ~year, y = ~incidence, type='scatter', mode='lines', line = list(color = toRGB("#009E73"))) %>%
+    layout(yaxis=list(title='Annual Incidence (%)'),
+           xaxis=list(title='Year', type='category', categoryorder='trace'),
+           #title='Exacerbation rate over time (across population)',
+           hovermode='x') #%>% config(displaylogo=F, modeBarButtonsToRemove=buttonremove)
+
+  return(p)
+}
+
+# Annual exacerbation rate over 20 years all population by age group
+report_exacerbation_by_time_entire_age <- function(exacerbation){
+
+  opx <- exacerbation$opx
+
+  g1 <- 41:55
+  g2 <- 56:70
+  g3 <- 71:84
+  g4 <- 85:111
+
+  a <- opx$n_exac_by_ctime_age
+  b <- opx$n_alive_by_ctime_age
+  y1 <- rowSums(a[, g1])/rowSums(b[, g1])
+  y2 <- rowSums(a[, g2])/rowSums(b[, g2])
+  y3 <- rowSums(a[, g3])/rowSums(b[, g3])
+  y4 <- rowSums(a[, g4])/rowSums(b[, g4])
+  max_y <- max(c(y1, y2, y3, y4))
+
+  x <- 2015:2034
+
+  data <- data.frame(x,y1,y2,y3,y4)
+  names(data) <- c("year", "y1", "y2", "y3", "y4")
+
+  p <- plot_ly(data, x= ~year, y = ~y1, type='scatter', name='40-54',mode='lines', line = list(color = toRGB("#009E73"))) %>%
+    add_trace(y = ~y2, name='55-64', line = list(color = toRGB("#E69F00"))) %>%
+    add_trace(y = ~y3, name='65-74', line = list(color = toRGB("#D55E00"))) %>%
+    add_trace(y = ~y4, name='75+', line = list(color = toRGB("#4286f4"))) %>%
+    layout(yaxis=list(title='Annual Incidence (%)'),
+           xaxis=list(title='Year', type='category', categoryorder='trace'),
+           title='Exacerbation rate by age group (across population)',
+           hovermode='x') #%>% config(displaylogo=F, modeBarButtonsToRemove=buttonremove)
+  return(p)
+}
+
+# Annual exacerbation rate over 20 years just COPD population
+report_exacerbation_by_time_copd_all <- function(exacerbation){
+
+  opx <- exacerbation$opx
+  y <- rowSums(opx$n_exac_by_ctime_severity)/rowSums(opx$n_COPD_by_ctime_sex)
+
+  x <- 2015:2034
+
+  data <- data.frame(x,y)
+  names(data) <- c("year", "incidence")
+  p <- plot_ly(data, x= ~year, y = ~incidence, type='scatter', mode='lines', line = list(color = toRGB("#009E73"))) %>%
+    layout(yaxis=list(title='Annual Incidence (%)'),
+           xaxis=list(title='Year', type='category', categoryorder='trace'),
+           title='Exacerbation rate over time (within COPD)',
+           hovermode='x') #%>% config(displaylogo=F, modeBarButtonsToRemove=buttonremove)
+
+  return(p)
+
+}
+
+#Annual exacerbation rate over 20 years just COPD by age group
+report_exacerbation_by_time_copd_age <- function(exacerbation){
+
+  opx <- exacerbation$opx
+
+  g1 <- 41:55
+  g2 <- 56:70
+  g3 <- 71:84
+  g4 <- 85:111
+
+  a <- opx$n_exac_by_ctime_age
+  b <- opx$n_COPD_by_ctime_age
+  y1 <- rowSums(a[, g1])/rowSums(b[, g1])
+  y2 <- rowSums(a[, g2])/rowSums(b[, g2])
+  y3 <- rowSums(a[, g3])/rowSums(b[, g3])
+  y4 <- rowSums(a[, g4])/rowSums(b[, g4])
+  max_y <- max(c(y1, y2, y3, y4))
+
+  x <- 2015:2034
+
+  data <- data.frame(x,y1,y2,y3,y4)
+  names(data) <- c("year", "y1", "y2", "y3", "y4")
+
+  p <- plot_ly(data, x= ~year, y = ~y1, type='scatter', name='40-54',mode='lines', line = list(color = toRGB("#009E73"))) %>%
+    add_trace(y = ~y2, name='55-64', line = list(color = toRGB("#E69F00"))) %>%
+    add_trace(y = ~y3, name='65-74', line = list(color = toRGB("#D55E00"))) %>%
+    add_trace(y = ~y4, name='75+', line = list(color = toRGB("#4286f4"))) %>%
+    layout(yaxis=list(title='Annual Incidence (%)'),
+           xaxis=list(title='Year', type='category', categoryorder='trace'),
+           title='Exacerbation rate by age group (within COPD)',
+           hovermode='x') #%>% config(displaylogo=F, modeBarButtonsToRemove=buttonremove)
+  return(p)
+}
+
+# Annual exacerbation rate over 20 years just COPD by exacerbation severity
+report_exacerbation_by_time_copd_severity <- function(exacerbation){
+
+  opx <- exacerbation$opx
+  a <- opx$n_exac_by_ctime_severity
+  b <- opx$n_COPD_by_ctime_sex
+  y1 <- (a[, 1])/rowSums(b)
+  y2 <- (a[, 2])/rowSums(b)
+  y3 <- (a[, 3] + a[, 4])/rowSums(b)
+  max_y <- max(c(y1, y2, y3))
+  plot(2015:2034, y1, type = "l", ylim = c(0, max_y * 1.5), xlab = "Year", ylab = "Annual rate", col = "green")
+  lines(2015:2034, y2, type = "l", col = "blue")
+  lines(2015:2034, y3, type = "l", col = "black")
+
+  legend("topright", c("mild", "moderate", "severe and very severe"), lty = c(1, 1, 1, 1), col = c("green", "blue", "black"))
+  title(cex.main = 1, "Exacerbation rate by exacerbation severity (within COPD)")
+
+  pie(colSums(opx$n_exac_by_ctime_severity), labels = c("Mild", "Moderate", "severe", "very severe"))
+
+  cat("Proportion by exacerbation severity:", format(colSums(opx$n_exac_by_ctime_severity/sum(opx$n_exac_by_ctime_severity)),
+                                                     digits = 2), "\n")
+  #terminate_session()
+}
+
+# Annual exacerbation rate over 20 years just COPD by GOLD stage
+report_exacerbation_by_time_copd_gold <- function(n_sim=10^5){
+
+  init_session()
+  run(n_sim)
+  all_events <- as.data.frame(Cget_all_events_matrix())
+  exac_events <- subset(all_events, event == 5)
+  exit_events <- subset(all_events, event == 14)
+
+  Follow_up_Gold <- c(0, 0, 0, 0)
+  last_GOLD_transition_time <- 0
+  for (i in 2:dim(all_events)[1]) {
+    if (all_events[i, "id"] != all_events[i - 1, "id"])
+      last_GOLD_transition_time <- 0
+    if ((all_events[i, "id"] == all_events[i - 1, "id"]) & (all_events[i, "gold"] != all_events[i - 1, "gold"])) {
+      Follow_up_Gold[all_events[i - 1, "gold"]] = Follow_up_Gold[all_events[i - 1, "gold"]] + all_events[i - 1, "followup_after_COPD"] -
+        last_GOLD_transition_time
+      last_GOLD_transition_time <- all_events[i - 1, "followup_after_COPD"]
+    }
+    if (all_events[i, "event"] == 14)
+      Follow_up_Gold[all_events[i, "gold"]] = Follow_up_Gold[all_events[i, "gold"]] + all_events[i, "followup_after_COPD"] -
+        last_GOLD_transition_time
+  }
+
+  terminate_session()
+
+  cat("Rates of exacerbation per GOLD stage:\n")
+  cat("GOLD I: ", as.data.frame(table(exac_events[, "gold"]))[1, 2]/Follow_up_Gold[1], "\n")
+  cat("GOLD II: ", as.data.frame(table(exac_events[, "gold"]))[2, 2]/Follow_up_Gold[2], "\n")
+  cat("GOLD III and IV :", (as.data.frame(table(exac_events[, "gold"]))[3, 2] + as.data.frame(table(exac_events[, "gold"]))[4,
+                                                                                                                            2])/(Follow_up_Gold[3] + Follow_up_Gold[4]), "\n")
+  terminate_session()
+}
--- epicRapp/app.R
+++ epicRapp/app.R
@@ -8,6 +8,9 @@
 library(shiny)
 library(ggplot2)
 library(plotly)
+library(shinydashboard)
+library(epicR)
+devtools::document()
 
 
 options(shiny.error = function() {
@@ -28,14 +31,19 @@ ui <- fluidPage(
   mainPanel(
     tabsetPanel(
       tabPanel("Annual Exacerbation Rates",
-               sidebarLayout(
-               sidebarPanel(
+               fluidRow(
+               box(id="sidebarpop",
                  selectInput('population', 'Population', c('Entire population', 'COPD population')),
-                 width = 5,
+                 width = 4,
                  uiOutput("age.severity")
                ),
-               mainPanel()
-               ))
+               box(id="mainpanelgraph",
+                   title= "Exacerbation rate over time (across population)",status="primary",
+                  plotlyOutput("age.severity.plots"), width=8
+               )
+
+
+              ))
     )
   )
 
@@ -46,6 +54,7 @@ ui <- fluidPage(
 server <- function(input, output, session) {
 
   popOptions <- c("Entire population", "COPD population")
+  groupOptions <- c("All", "By Age Group", "By Exacerbation Severity", "By GOLD Stage")
 
   output$age.severity <- renderUI({
 
@@ -57,7 +66,35 @@ server <- function(input, output, session) {
       list(selectInput('group', "Patient Group", c("All", "By Age Group", "By Exacerbation Severity", "By GOLD Stage")))
     }
 
-    })}
+  })
+
+  output$age.severity.plots <- renderPlotly({
+
+    exacerbation <- report_exacerbation_by_time_base()
+
+    if (input$population==popOptions[1]) {
+
+      if(input$group==groupOptions[1]){
+
+        report_exacerbation_by_time_entire_all(exacerbation)
+
+      } else if (input$group==groupOptions[2]) {
+
+        report_exacerbation_by_time_entire_age(exacerbation)
+      }
+
+    } else if (input$population==popOptions[2]) {
+
+      if(input$group==groupOptions[1]){
+        report_exacerbation_by_time_copd_all(exacerbation)
+
+      } else if (input$group==groupOptions[2]){
+        report_exacerbation_by_time_copd_age(exacerbation)
+      }
+    }
+  })
+
+  }
 
 shinyApp(ui = ui, server = server)
 
