3acf8d9609fcbd3110c298f52f1bff549095dfa6
Adibi <maadibi@ead.ubc.ca>
2018-10-03 17:07:06 -0700

    Rcpp Armadillo added for mvrnorm in C++

--- src/model.WIP.cpp
+++ src/model.WIP.cpp
@@ -27,7 +27,8 @@ Layout:
 #define MAX_AGE 111
 
 
-#include <Rcpp.h>
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
 using namespace Rcpp;
 
 
@@ -149,6 +150,12 @@ int last_id;
 
 
 
+// [[Rcpp::export]]
+arma::mat mvrnormArma(int n, arma::vec mu, arma::mat sigma) {
+  int ncols = sigma.n_cols;
+  arma::mat Y = arma::randn(n, ncols);
+  return arma::repmat(mu, 1, n).t() + Y * arma::chol(sigma);
+}
 
 NumericMatrix array_to_Rmatrix(std::vector<double> x, int nCol)
 {
