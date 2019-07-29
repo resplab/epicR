e8271d4057e7236f527d99281fd6712c6271ae72
Johnson <katemj91@ead.ubc.ca>
2019-07-29 16:51:25 -0700

    added overdiagnosis to diagnosis pathway

--- src/model.WIP.cpp
+++ src/model.WIP.cpp
@@ -575,6 +575,7 @@ struct input
     double logit_p_diagnosis_by_sex[10][2];
     double p_hosp_diagnosis;
     double logit_p_overdiagnosis_by_sex[9][2];
+    double p_correct_overdiagnosis;
     double p_case_detection;
     double min_cd_age;
     double min_cd_pack_years;
@@ -746,6 +747,7 @@ List Cget_inputs()
     Rcpp::Named("logit_p_diagnosis_by_sex")=AS_MATRIX_DOUBLE(input.diagnosis.logit_p_diagnosis_by_sex),
     Rcpp::Named("p_hosp_diagnosis")=input.diagnosis.p_hosp_diagnosis,
     Rcpp::Named("logit_p_overdiagnosis_by_sex")=AS_MATRIX_DOUBLE(input.diagnosis.logit_p_overdiagnosis_by_sex),
+    Rcpp::Named("p_correct_overdiagnosis")=input.diagnosis.p_correct_overdiagnosis,
     Rcpp::Named("p_case_detection")=input.diagnosis.p_case_detection,
     Rcpp::Named("min_cd_age")=input.diagnosis.min_cd_age,
     Rcpp::Named("min_cd_pack_years")=input.diagnosis.min_cd_pack_years,
@@ -870,6 +872,7 @@ int Cset_input_var(std::string name, NumericVector value)
   if(name=="diagnosis$logit_p_diagnosis_by_sex") READ_R_MATRIX(value,input.diagnosis.logit_p_diagnosis_by_sex);
   if(name=="diagnosis$p_hosp_diagnosis") {input.diagnosis.p_hosp_diagnosis=value[0]; return(0);};
   if(name=="diagnosis$logit_p_overdiagnosis_by_sex") READ_R_MATRIX(value,input.diagnosis.logit_p_overdiagnosis_by_sex);
+  if(name=="diagnosis$p_correct_overdiagnosis") {input.diagnosis.p_correct_overdiagnosis=value[0]; return(0);};
   if(name=="diagnosis$p_case_detection") {input.diagnosis.p_case_detection=value[0]; return(0);};
   if(name=="diagnosis$min_cd_age") {input.diagnosis.min_cd_age=value[0]; return(0);};
   if(name=="diagnosis$min_cd_pack_years") {input.diagnosis.min_cd_pack_years=value[0]; return(0);};
@@ -1012,12 +1015,12 @@ struct agent
   bool dyspnea;
   bool wheeze;
 
-  double gpvisits;
+  int gpvisits;
   double tmp_gpvisits_rate;
   int diagnosis;
   double p_hosp_diagnosis;
-  int overdiagnosis;
-  double case_detection;
+  double p_correct_overdiagnosis;
+  int case_detection;
   double p_case_detection;
   double min_cd_age;
   double min_cd_pack_years;
@@ -1135,7 +1138,6 @@ List get_agent(agent *ag)
   out["gpvisits"] = (*ag).gpvisits;
   out["tmp_gpvisits_rate"] = (*ag).tmp_gpvisits_rate;
   out["diagnosis"] = (*ag).diagnosis;
-  out["overdiagnosis"] = (*ag).overdiagnosis;
   out["case_detection"] = (*ag).case_detection;
 
   out["tmp_exac_rate"] = (*ag).tmp_exac_rate;
@@ -1637,8 +1639,8 @@ void update_output_ex(agent *ag)
       output_ex.n_COPD_by_ctime_age[time][age-1]+=((*ag).gold>0)*1;
       output_ex.n_COPD_by_ctime_severity[time][((*ag).gold)]+=1;
       output_ex.n_COPD_by_age_sex[age-1][(*ag).sex]+=1;
-      output_ex.n_Diagnosed_by_ctime_sex[time][(*ag).sex]+=((*ag).diagnosis>0)*1;
-      output_ex.n_Overdiagnosed_by_ctime_sex[time][(*ag).sex]+=((*ag).overdiagnosis>0)*1;
+      if((*ag).gold>0) output_ex.n_Diagnosed_by_ctime_sex[time][(*ag).sex]+=((*ag).diagnosis>0)*1;
+      if((*ag).gold==0) output_ex.n_Overdiagnosed_by_ctime_sex[time][(*ag).sex]+=((*ag).diagnosis>0)*1;
       if((*ag).gold>0) output_ex.n_Diagnosed_by_ctime_severity[time][(*ag).gold]+=((*ag).diagnosis>0)*1;
       if((*ag).local_time>0) output_ex.cumul_time_by_ctime_GOLD [time][((*ag).gold)]+=1;
 
@@ -1837,16 +1839,16 @@ double apply_case_detection(agent *ag)
  double update_diagnosis(agent *ag)
 {
 
-   if((*ag).diagnosis>0) return(0);
-
   double p_diagnosis = 0;
 
   if ((*ag).gpvisits!=0) {
 
   apply_case_detection(ag);
 
-  if((*ag).gold!=0)
-  {
+  if((*ag).gold!=0)  {
+
+    if((*ag).diagnosis>0) return(0);
+
     p_diagnosis = exp(input.diagnosis.logit_p_diagnosis_by_sex[0][(*ag).sex] +
       input.diagnosis.logit_p_diagnosis_by_sex[1][(*ag).sex]*((*ag).local_time+(*ag).age_at_creation) +
       input.diagnosis.logit_p_diagnosis_by_sex[2][(*ag).sex]*((*ag).smoking_status) +
@@ -1865,45 +1867,51 @@ double apply_case_detection(agent *ag)
             (*ag).medication_status=MED_CLASS_LAMA;
             medication_LPT(ag);
           }
+
+  } else {
+
+    double correct_overdiagnosis;
+    correct_overdiagnosis = input.diagnosis.p_correct_overdiagnosis;
+
+      if((*ag).diagnosis>0) {
+
+        if(rand_unif() < correct_overdiagnosis) {
+
+        (*ag).diagnosis = 0;
+
+          return(0);
+      }
+
+    } else {
+
+      double p_overdiagnosis = 0;
+
+    p_overdiagnosis = exp(input.diagnosis.logit_p_overdiagnosis_by_sex[0][(*ag).sex] +
+      input.diagnosis.logit_p_overdiagnosis_by_sex[1][(*ag).sex]*((*ag).local_time+(*ag).age_at_creation) +
+      input.diagnosis.logit_p_overdiagnosis_by_sex[2][(*ag).sex]*((*ag).smoking_status) +
+      input.diagnosis.logit_p_overdiagnosis_by_sex[3][(*ag).sex]*((*ag).gpvisits) +
+      input.diagnosis.logit_p_overdiagnosis_by_sex[4][(*ag).sex]*((*ag).cough) +
+      input.diagnosis.logit_p_overdiagnosis_by_sex[5][(*ag).sex]*((*ag).phlegm) +
+      input.diagnosis.logit_p_overdiagnosis_by_sex[6][(*ag).sex]*((*ag).wheeze) +
+      input.diagnosis.logit_p_overdiagnosis_by_sex[7][(*ag).sex]*((*ag).dyspnea) +
+      input.diagnosis.logit_p_overdiagnosis_by_sex[8][(*ag).sex]*((*ag).case_detection));
+
+    p_overdiagnosis = p_overdiagnosis / (1 + p_overdiagnosis);
+
+    if (rand_unif() < p_overdiagnosis) {
+      (*ag).diagnosis = 1;
+
+    } else {
+
+      (*ag).diagnosis = 0;
+
         }
       }
-  return(0);
- }
-
-
-///////// Overdiagnosis ///////////
-
-double update_overdiagnosis(agent *ag)
-{
-  if((*ag).gold==0) {
-
-  if((*ag).overdiagnosis>0) return(0);
-
-  double p_overdiagnosis = 0;
-
-  if ((*ag).gpvisits!=0) {
-
-      p_overdiagnosis = exp(input.diagnosis.logit_p_overdiagnosis_by_sex[0][(*ag).sex] +
-            input.diagnosis.logit_p_overdiagnosis_by_sex[1][(*ag).sex]*((*ag).local_time+(*ag).age_at_creation) +
-            input.diagnosis.logit_p_overdiagnosis_by_sex[2][(*ag).sex]*((*ag).smoking_status) +
-            input.diagnosis.logit_p_overdiagnosis_by_sex[3][(*ag).sex]*((*ag).gpvisits) +
-            input.diagnosis.logit_p_overdiagnosis_by_sex[4][(*ag).sex]*((*ag).cough) +
-            input.diagnosis.logit_p_overdiagnosis_by_sex[5][(*ag).sex]*((*ag).phlegm) +
-            input.diagnosis.logit_p_overdiagnosis_by_sex[6][(*ag).sex]*((*ag).wheeze) +
-            input.diagnosis.logit_p_overdiagnosis_by_sex[7][(*ag).sex]*((*ag).dyspnea) +
-            input.diagnosis.logit_p_overdiagnosis_by_sex[8][(*ag).sex]*((*ag).case_detection));
-
-      p_overdiagnosis = p_overdiagnosis / (1 + p_overdiagnosis);
-
-      if (rand_unif() < p_overdiagnosis) {
-        (*ag).overdiagnosis = 1;
-            }
-        }
-    } else {
-      (*ag).overdiagnosis = 0;
     }
+  }
   return(0);
 }
+
 
 
 ////
@@ -1938,7 +1946,6 @@ double _bvn[2]; //being used for joint estimation in multiple locations;
 (*ag).gpvisits  = 0;
 (*ag).tmp_gpvisits_rate  = 0;
 (*ag).diagnosis = 0;
-(*ag).overdiagnosis = 0;
 (*ag).case_detection = 0;
 
 (*ag).tmp_exac_rate = 0;
@@ -2534,8 +2541,8 @@ DataFrame Cget_all_events() //Returns all events from all agents;
 // [[Rcpp::export]]
 NumericMatrix Cget_all_events_matrix()
 {
-  NumericMatrix outm(event_stack_pointer,31);
-  CharacterVector eventMatrixColNames(31);
+  NumericMatrix outm(event_stack_pointer,30);
+  CharacterVector eventMatrixColNames(30);
 
 // eventMatrixColNames = CharacterVector::create("id", "local_time","sex", "time_at_creation", "age_at_creation", "pack_years","gold","event","FEV1","FEV1_slope", "FEV1_slope_t","pred_FEV1","smoking_status", "localtime_at_COPD", "age_at_COPD", "weight_at_COPD", "height","followup_after_COPD", "FEV1_baseline");
 // 'create' helper function is limited to 20 enteries
@@ -2567,10 +2574,9 @@ NumericMatrix Cget_all_events_matrix()
   eventMatrixColNames(24) = "gpvisits";
   eventMatrixColNames(25) = "tmp_gpvisits_rate";
   eventMatrixColNames(26) = "diagnosis";
-  eventMatrixColNames(27) = "overdiagnosis";
-  eventMatrixColNames(28) = "medication_status";
-  eventMatrixColNames(29) = "tmp_exac_rate";
-  eventMatrixColNames(30) = "case_detection";
+  eventMatrixColNames(27) = "medication_status";
+  eventMatrixColNames(28) = "tmp_exac_rate";
+  eventMatrixColNames(29) = "case_detection";
 
   colnames(outm) = eventMatrixColNames;
   for(int i=0;i<event_stack_pointer;i++)
@@ -2603,10 +2609,9 @@ NumericMatrix Cget_all_events_matrix()
     outm(i,24)=(*ag).gpvisits;
     outm(i,25)=(*ag).tmp_gpvisits_rate;
     outm(i,26)=(*ag).diagnosis;
-    outm(i,27)=(*ag).overdiagnosis;
-    outm(i,28)=(*ag).medication_status;
-    outm(i,29)=(*ag).tmp_exac_rate;
-    outm(i,30)=(*ag).case_detection;
+    outm(i,27)=(*ag).medication_status;
+    outm(i,28)=(*ag).tmp_exac_rate;
+    outm(i,29)=(*ag).case_detection;
 
   }
 
@@ -3301,7 +3306,6 @@ agent *event_fixed_process(agent *ag)
   update_symptoms(ag); //updating symptoms in the annual event
   update_gpvisits(ag); //updating gp visits in the annual event
   update_diagnosis(ag); //updating diagnosis in the annual event
-  update_overdiagnosis(ag);
 
 #ifdef OUTPUT_EX
   update_output_ex(ag);
