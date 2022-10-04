d373330e227795e55536135dbf7d4888a18acedf
rachaelmountain <r.gallop@lancaster.ac.uk>
2022-10-04 13:59:25 -0700

    Minor changes

--- src/model.cpp
+++ src/model.cpp
@@ -1703,7 +1703,7 @@ void update_output_ex(agent *ag)
       if((*ag).gold>0) output_ex.n_Diagnosed_by_ctime_sex[time][(*ag).sex]+=((*ag).diagnosis>0)*1;
       if((*ag).gold==0) output_ex.n_Overdiagnosed_by_ctime_sex[time][(*ag).sex]+=((*ag).diagnosis>0)*1;
       if((*ag).gold>0) output_ex.n_Diagnosed_by_ctime_severity[time][(*ag).gold]+=((*ag).diagnosis>0)*1;
-      if((*ag).case_detection>0) output_ex.n_case_detection_by_ctime[time][(*ag).case_detection - 1]+=1;;
+      if((*ag).case_detection>0) output_ex.n_case_detection_by_ctime[time][(*ag).case_detection]+=1;;
       if((*ag).local_time>0) output_ex.cumul_time_by_ctime_GOLD[time][((*ag).gold)]+=1;
 #endif
 
@@ -2011,7 +2011,9 @@ double update_prevalent_diagnosis(agent *ag)
         (*ag).cumul_cost+=input.cost.cost_gp_visit/pow(1+input.global_parameters.discount_cost,(*ag).local_time+calendar_time);
         (*ag).time_at_diagnosis=(*ag).local_time;
         (*ag).smoking_at_diagnosis=(*ag).smoking_status;
-        if((*ag).case_detection==1) {(*ag).case_detection=2)};
+        if((*ag).case_detection==1) {
+          (*ag).case_detection=2)
+          };
 
       }
 
