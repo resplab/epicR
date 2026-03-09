1c27c0a96fdae5655c7e89499808f090adc34885
amin <adibi@alumni.ubc.ca>
2026-03-09 11:58:53 -0700

    validate_survival() fixed

--- R/epicR.R
+++ R/epicR.R
@@ -16,11 +16,11 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c(".",
   "GOLD1", "GOLD4", "Incidence", "Male", "Mild", "NoCOPD", "NonCOPD", "Number", "Overdiagnosed",
   "Prevalence", "Proportion", "Rate", "Sex", "Undiagnosed", "Variable", "VerySevere", "Visits",
   "Year", "Year_with_COPD", "age", "age_at_creation", "age_coeff_men", "age_coeff_women",
-  "cox.zph", "coxph", "diagnosis", "event", "exac_status", "female", "followup_after_COPD",
+  "diagnosis", "event", "exac_status", "female", "followup_after_COPD",
   "gold", "intercept_men", "intercept_women", "iteration", "local_time",
   "medication_status", "pack_years", "packyears_coeff_men",
   "packyears_coeff_women", "population", "prop", "resid_age_coeff_men",
   "resid_age_coeff_women", "resid_intercept_men", "resid_intercept_women",
   "resid_packyears_coeff_men", "resid_packyears_coeff_women", "settings",
-  "smoking_status", "theme_cleantable", "time_at_creation", "value", "variable"
+  "smoking_status", "time_at_creation", "value", "variable"
 ))
