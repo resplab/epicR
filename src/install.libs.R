7f4716382be10d1f916ef525e8c99766dfaa3c2e
amin <adibi@alumni.ubc.ca>
2026-03-09 14:51:35 -0700

    fix for windows

--- src/install.libs.R
+++ src/install.libs.R
@@ -2,6 +2,6 @@ files <- Sys.glob(paste0("*", SHLIB_EXT))
 dest <- file.path(R_PACKAGE_DIR, paste0("libs", R_ARCH))
 dir.create(dest, recursive = TRUE, showWarnings = FALSE)
 file.copy(files, dest, overwrite = TRUE)
-if (nzchar(Sys.which("strip"))) {
+if (.Platform$OS.type != "windows" && nzchar(Sys.which("strip"))) {
   system2("strip", c("-x", file.path(dest, files)))
 }
